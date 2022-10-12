60252741f97db77162d8c9aee7a5c9ce09afd586
Aju Thomas <atajuapache@gmail.com>
2022-10-12 19:27:06 +1030

    created an account and create an API key

---
+++ Assets/script.js
@@ -0,0 +1,11 @@
+
+var cityName = document.querySelector("#cityName");
+
+
+var apiKey = "403d92c0f500944e93ced3dd4c0c88f8";
+
+function weatherDetails(cityName){
+    var webUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+
+    
+}
